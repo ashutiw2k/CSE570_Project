0e8b612af10028cecd740ef5cd2c89d74badfc77
Ashutosh Tiwari <ashutiw2k@gmail.com>
2024-12-10 16:28:59 -0500

    Add timestamp sequence lists.

--- project_main/src/save_timestamps_in_sequence.py
+++ project_main/src/save_timestamps_in_sequence.py
@@ -1,3 +1,17 @@
 import json
+import os
 
-ALL_SEQUENCE_PATH = 'Data/RAN4model_dfv4p4/seqs/indoor/scene0'
+ALL_SEQUENCE_PATH = 'Data/RAN4model_dfv4p4/seqs/indoor/scene0/'
+SEQUENCE_WRITE_PATH = 'data/'
+
+ctr = 0
+write_dict = {}
+for seq in os.listdir(ALL_SEQUENCE_PATH):
+    seq_path = ALL_SEQUENCE_PATH + seq
+    write_dict.update({
+        f'seq{ctr}': list(os.listdir(seq_path))
+    })
+    ctr += 1
+
+with open(SEQUENCE_WRITE_PATH+'files_in_sequence.json') as f:
+    json.dump(write_dict, f)
