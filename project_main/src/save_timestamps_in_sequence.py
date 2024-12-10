3cd3a89278dd281906ebf18fa45a7ab88dd8a592
Ashutosh Tiwari <ashutiw2k@gmail.com>
2024-12-10 16:45:17 -0500

    JSON for image names per sequence

--- project_main/src/save_timestamps_in_sequence.py
+++ project_main/src/save_timestamps_in_sequence.py
@@ -2,18 +2,18 @@ import json
 import os
 
 ALL_SEQUENCE_PATH = 'Data/RAN4model_dfv4p4/seqs/indoor/scene0/'
-SEQUENCE_WRITE_PATH = 'data/'
-
+SEQUENCE_WRITE_PATH = 'project_main/data/'
+IMAGE_PATH = 'RGB_anonymized/'
 ctr = 0
 write_dict = {}
-for seq in os.listdir(ALL_SEQUENCE_PATH):
-    seq_path = ALL_SEQUENCE_PATH + seq
+for seq in sorted(os.listdir(ALL_SEQUENCE_PATH)):
+    seq_path = ALL_SEQUENCE_PATH + seq + '/' + IMAGE_PATH
     write_dict.update({
         f'seq{ctr}': { 
             'sequence' : seq,
-            'timestamps':['.'.join(p.split('.')[0:2]) for p in os.listdir(seq_path)] }
+            'timestamps':['.'.join(p.split('.')[0:2]) for p in sorted(os.listdir(seq_path)) if p.endswith('.png')] }
     })
     ctr += 1
 
-with open(SEQUENCE_WRITE_PATH+'files_in_sequence.json') as f:
+with open(SEQUENCE_WRITE_PATH+'files_in_sequence.json', 'w') as f:
     json.dump(write_dict, f)
