2fe3c06a35ee45ce37aed194a9957a4825161f2d
Ashutosh Tiwari <ashutiw2k@gmail.com>
2024-12-11 03:06:57 -0500

    Plot circles pipeline

--- project_main/src/plot_result_on_image.py
+++ project_main/src/plot_result_on_image.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
 import cv2
+from tqdm import tqdm
+import numpy as np
+
 
 PREDICTIONS_DIR = 'project_main/predictions/'
 PREDICTED_FRAMES = 'PredictedFrames/'
@@ -9,15 +12,54 @@ PRED_CSV = 'predictions.csv'
 
 def annotate_image_with_centriods(true, pred, readpath, writepath):
     image = cv2.imread(readpath)
+    # true_x, true_y = true
+    # pred_x, pred_y = pred
+    # true_center_coordinates = (200, 300)  # (x, y)
+    radius = 60
+    true_color = (0, 0, 255)  # Red in BGR
+    pred_color = (255, 0, 0)
+    thickness = 2  # Line thickness
+
+    # print(true, pred)
+
+    # return
+
+    cv2.circle(image, true, radius, true_color, thickness)
+    cv2.circle(image, pred, radius, pred_color, thickness)
+
+    # if os.path.isfile(write_img):
+    #     raise FileExistsError(f'Trying to write to {write_img} that already exists')
+    cv2.imwrite(writepath, image)
 
 
-for subject in os.listdir(PREDICTIONS_DIR):
-    sub_ann_frames = ANNOTATED_FRAMES + subject + '/'
-    sub_out_frames = PREDICTIONS_DIR + subject + '/' + PREDICTED_FRAMES
-    sub_csv = PREDICTIONS_DIR + subject + '/' + PRED_CSV
 
-    # with open(sub_csv, 'r') as f:
-    sub_pred_df = pd.read_csv(sub_csv)
+if __name__ == '__main__':
+
+    for subject in os.listdir(PREDICTIONS_DIR):
+        if os.path.isfile(PREDICTIONS_DIR + subject):
+            continue
+
+        sub_ann_frames = ANNOTATED_FRAMES + subject + '/'
+        sub_out_frames = PREDICTIONS_DIR + subject + '/' + PREDICTED_FRAMES
+        sub_csv = PREDICTIONS_DIR + subject + '/' + PRED_CSV
+
+        # with open(sub_csv, 'r') as f:
+        sub_pred_df = pd.read_csv(sub_csv)
+
+        for row in tqdm(sub_pred_df.itertuples(index=False), desc=subject):
+            read_img = ANNOTATED_FRAMES + subject + '/' + row[0] + '.png'
+            write_img = sub_out_frames + row[0] + '.png'
+            # print(row[2].type)
+            true_vals = np.array((row[1], row[3]))
+            pred_vals = np.array((row[2], row[4]))
+
+            true_vals = tuple(map(int, np.round(true_vals)))
+            pred_vals = tuple(map(int, np.round(pred_vals)))
+
+            annotate_image_with_centriods(true_vals, pred_vals, readpath=read_img, writepath=write_img)
+
+            # print(read_img, write_img)
+
 
 
 
